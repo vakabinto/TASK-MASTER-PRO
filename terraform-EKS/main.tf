e62b80aacb5dbe8e7a49f39e4a6488f6cea63362
vakabinto <vakabinto@example.com>
2024-08-18 08:00:39 +0100

    update

--- terraform-EKS/main.tf
+++ terraform-EKS/main.tf
@@ -73,13 +73,15 @@ resource "aws_iam_role" "eks_cluster_role" {
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "eks.amazonaws.com"
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
       }
-    }]
+    ]
   })
 }
 
@@ -118,13 +120,15 @@ resource "aws_iam_role" "eks_node_role" {
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "ec2.amazonaws.com"
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
       }
-    }]
+    ]
   })
 }
 
@@ -136,7 +140,7 @@ resource "aws_iam_role_policy_attachment" "eks_node_role_policy" {
 
 resource "aws_iam_role_policy_attachment" "eks_node_role_cni" {
   role       = aws_iam_role.eks_node_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSCNIPolicy"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
 
 resource "aws_iam_role_policy_attachment" "eks_node_role_autoscaling" {
